538c03578488102a571ca3b8441f94674575f4b3
Mark-Nelson <mark.nelson@dragonicity.com>
2016-06-29 12:09:17 +0800

    Tickets can have a state assigned when comments are created

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,14 @@ end
     Project.create!(name: project, description: "Sample project for #{project}")
   end
 end
+
+# Seed ticket states
+
+unless State.exists?
+  State.create(name: "New",     color: "#0066CC")
+  State.create(name: "Open",    color: "#008000")
+  State.create(name: "Closed",  color: "#990000")
+  State.create(name: "Awesome", color: "#663399")
+end
+
+
