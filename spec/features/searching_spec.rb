6562a2ac58dca4f1a54b0dc39f7caefd4fe452f0
Mark-Nelson <mark.nelson@dragonicity.com>
2016-07-01 15:03:49 +0800

    More problems with Searcher gem

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,14 +3,17 @@ require "rails_helper"
 RSpec.feature "Users can search for tickets matching specific criteria" do 
   let(:user)    { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project) }
+  let(:open)    { State.create(name: "Open", default: true) }
+  let(:closed)  { State.create(name: "Closed") }
+
   let!(:ticket_1) do
     FactoryGirl.create(:ticket, name: "Create projects",
-      project: project, author: user, tag_names: "iteration_1")
+      project: project, author: user, tag_names: "iteration_1", state: open)
   end
 
   let!(:ticket_2) do
     FactoryGirl.create(:ticket, name: "Create users",
-      project: project, author: user, tag_names: "iteration_2")
+      project: project, author: user, tag_names: "iteration_2", state: closed)
   end
 
   before do
@@ -27,4 +30,15 @@ RSpec.feature "Users can search for tickets matching specific criteria" do
       expect(page).to_not have_link "Create users"
     end
   end
+
+  #Code below does not work due to some issue with Searcher gem I believe
+
+  # scenario "searching by state" do
+  #   fill_in "Search", with: "state:Open"
+  #   click_button "Search"
+  #   within("#tickets") do
+  #     expect(page).to have_link "Create projects"
+  #     expect(page).not_to have_link "Create users"
+  #   end
+  # end
 end
